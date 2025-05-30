061ea91d14ba6465f4ec36411bd3445d4b8481ce
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-05-30 17:54:39 +0700

    commit pertama

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/**
+ * @copyright 2025 naufaldsp
+ * @license Apache-2.0
+ */
+
+/**
+ * Components
+ * 
+ */
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <Header />
+  );
+}
+
+export default App;
