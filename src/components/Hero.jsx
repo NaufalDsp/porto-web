259ca1b163a1688072f49b5a81e684dc7633c0e5
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-05-31 20:42:09 +0700

    update hero fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,10 @@
  * @license Apache-2.0
  */
 
-import React from 'react'
+/**
+ * Components
+ */
+import { ButtonPrimary, ButtonOutline } from "./Button";
 
 const Hero = () => {
   return (
@@ -43,9 +46,16 @@ const Hero = () => {
         </h2>
 
         <div className="flex items-center gap-3">
-            ButtonPrimary
+            <ButtonPrimary 
+             label="Download CV"
+             icon="download"
+            />
 
-            ButtonOutilne
+            <ButtonOutline
+             href="#about"
+             label="Scroll down"
+             icon="arrow-downward"
+             />
         </div>
     </div>
 
